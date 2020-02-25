cf635d6c86c2f87449f5e80a771959c878f13d8a
ColorsWind <1748591300@qq.com>
2020-02-25 17:23:58 +0800

    改善 PacketMenuBuilder 的安全性.

--- common/src/main/java/com/blzeecraft/virtualmenu/core/menu/PacketMenuBuilder.java
+++ common/src/main/java/com/blzeecraft/virtualmenu/core/menu/PacketMenuBuilder.java
@@ -1,12 +1,16 @@
 package com.blzeecraft.virtualmenu.core.menu;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 
+import com.blzeecraft.virtualmenu.core.action.ActionUtils;
 import com.blzeecraft.virtualmenu.core.action.event.MenuEvent;
 import com.blzeecraft.virtualmenu.core.icon.Icon;
 import com.blzeecraft.virtualmenu.core.logger.LogNode;
+import com.blzeecraft.virtualmenu.core.user.UserSession;
 
 import lombok.NonNull;
 
@@ -53,7 +57,11 @@ public class PacketMenuBuilder {
 			return this;
 		}
 		throw new IllegalArgumentException("尝试访问菜单不存在的icon的位置, length=" + icons.length + " slot=" + slot);
+	}
 
+	public PacketMenuBuilder icon(Icon[] icons) {
+		this.icons = icons;
+		return this;
 	}
 
 	public PacketMenuBuilder addIcon(Icon icon) {
@@ -72,6 +80,17 @@ public class PacketMenuBuilder {
 	}
 
 	public PacketMenu build() {
+		// fill
+		if (icons.length != type.getSize()) {
+			this.icons = new Icon[type.getSize()];
+			System.arraycopy(icons, 0, this.icons, 0, this.icons.length);
+		}
+		IntStream.range(0, this.type.getSize()).forEach(i -> {
+			if (this.icons[i] == null) {
+				icons[i] = UserSession.EMPTY_ICON;
+			}
+		});
+		Arrays.stream(EventType.values()).forEach(eventType -> menuAction.putIfAbsent(eventType, ActionUtils.EMPTY_ACTION));
 		return new PacketMenu(node, refresh, title, type, icons, menuAction);
 	}
 
