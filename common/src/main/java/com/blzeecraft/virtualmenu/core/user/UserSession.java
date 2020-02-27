2ae1895c07464480a3b192e3f7768578d1ff18b6
ColorsWind <1748591300@qq.com>
2020-02-27 21:25:16 +0800

    更新 UserSession, 采用新的模式:
    1、Inventory的Item将在UserSession中保存一份副本, 通过监听 Packet 与同步.
    2、PacketMenu的Item将在UserSession中保存一份副本, 需要手动设置.

--- common/src/main/java/com/blzeecraft/virtualmenu/core/user/UserSession.java
+++ common/src/main/java/com/blzeecraft/virtualmenu/core/user/UserSession.java
@@ -1,14 +1,16 @@
 package com.blzeecraft.virtualmenu.core.user;
 
 import java.lang.reflect.Array;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicReferenceArray;
 
+import com.blzeecraft.virtualmenu.core.packet.AbstractPacketOutSetSlot;
+import com.blzeecraft.virtualmenu.core.packet.AbstractPacketOutWindowItems;
+import com.blzeecraft.virtualmenu.core.packet.AbstractPacketOutWindowOpen;
+import com.blzeecraft.virtualmenu.core.packet.IPacketAdapter;
 import com.blzeecraft.virtualmenu.core.packet.PacketManager;
 import com.blzeecraft.virtualmenu.core.VirtualMenu;
-import com.blzeecraft.virtualmenu.core.icon.EmptyIcon;
 import com.blzeecraft.virtualmenu.core.icon.Icon;
 import com.blzeecraft.virtualmenu.core.icon.MultiIcon;
 import com.blzeecraft.virtualmenu.core.item.AbstractItem;
@@ -19,6 +21,7 @@ import com.blzeecraft.virtualmenu.core.menu.IPacketMenu;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.val;
 
 /**
  * 用于保存玩家目前打开菜单的相关信息. 应在确认玩家打开菜单请求后, 创建发送给客户端的 Packet 前, 由 {@link PacketManager}
@@ -31,9 +34,9 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class UserSession {
 	public static final LogNode LOG_NODE = LogNode.of("#UserSession");
-	//public static final EmptyIcon EMPTY_ICON = new EmptyIcon();
 	public static final AbstractItem<?> EMPTY_ITEM = VirtualMenu.emptyItem();
 	public static final Object EMPTY_RAW_ITEM = EMPTY_ITEM.getHandle();
+	public static final Class<?> RAW_ITEM_TYPE = EMPTY_RAW_ITEM.getClass();
 	private static Object ensureNotNull(Object item) {
 		return item == null ? EMPTY_RAW_ITEM : item;
 	}
@@ -45,12 +48,16 @@ public class UserSession {
 	@Getter
 	@EqualsAndHashCode.Include
 	protected final IPacketMenu menu;
-
+	
+	protected final IPacketAdapter packetAdapter;
+	
 	
 	// 保存玩家 PacketMenu 的 RawItem
 	protected final AtomicReferenceArray<Object> packetMenuRawItem;
 	// 保存玩家 Inventory 的 RawItem
 	protected final AtomicReferenceArray<Object> inventoryRawItem;
+	// 保存玩家 MultiIcon 显示的 Icon
+	protected final ConcurrentMap<MultiIcon, Icon> viewIcon;
 
 	/**
 	 * 创建一个新的 UserSession 对象, 创建对象后还需要进行初始化 {@link #init(Icon[])}.
@@ -63,6 +70,8 @@ public class UserSession {
 		this.menu = menu;
 		this.packetMenuRawItem = new AtomicReferenceArray<>(menu.getSize());
 		this.inventoryRawItem = new AtomicReferenceArray<>(36);
+		this.viewIcon = new ConcurrentHashMap<>();
+		this.packetAdapter = VirtualMenu.getPacketAdapter();
 	}
 
 
@@ -82,16 +91,14 @@ public class UserSession {
 	/**
 	 * 获取 Inventory 缓存的所有 RawItem.
 	 * @param <T>
-	 * @param array
-	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public <T> T[] getCacheInventoryRawItems(T[] array) {
-		this.checkSizeInventory(array);
+	public <T> T[] getCacheInventoryRawItems() {
+		Object[] itemArray = (Object[]) Array.newInstance(RAW_ITEM_TYPE, inventoryRawItem.length());
 		for(int i=0;i<this.inventoryRawItem.length();i++) {
-			array[i] = (T) this.inventoryRawItem.get(i);
+			itemArray[i] = this.inventoryRawItem.get(i);
 		}
-		return array;
+		return (T[]) itemArray;
 	}
 	
 	/**
@@ -151,12 +158,12 @@ public class UserSession {
 	 * @return
 	 */
 	@SuppressWarnings("unchecked")
-	public <T> T[] getCachePacketMenuRawItems(T[] array) {
-		this.checkSizePacketMenu(array);
+	public <T> T[] getCachePacketMenuRawItems() {
+		Object[] itemArray = (Object[]) Array.newInstance(RAW_ITEM_TYPE, inventoryRawItem.length());
 		for(int i=0;i<this.packetMenuRawItem.length();i++) {
-			array[i] = (T) this.packetMenuRawItem.get(i);
+			itemArray[i] = this.packetMenuRawItem.get(i);
 		}
-		return array;
+		return (T[]) itemArray;
 	}
 	
 	/**
@@ -196,55 +203,54 @@ public class UserSession {
 		}
 	}
 	
-
-	
-
-	
-	
-	
-	
-
-
-	/**
-	 * 刷新指定 slot 的 Icon. 保证返回可用的结果.
-	 * 
-	 * @param slot
-	 * @param icon
-	 * @return 新的 Item
-	 */
-	public AbstractItem<?> updateItem(int slot, Icon icon) {
-		AbstractItem<?> item = icon.refreshItem(this);
-		fastAccess.set(slot, item);
-		return item;
+	public AbstractPacketOutWindowOpen<?> createPacketWindowOpen() {
+		val packetWindowOpen = packetAdapter.createPacketWindowOpen();
+		packetWindowOpen.setWindowId(menu.getWindowId());
+		packetWindowOpen.setMenuType(menu.getType());
+		packetWindowOpen.setTitle(menu.getTitle());
+		return packetWindowOpen;
 	}
-
-	/**
-	 * 初始化 UserSession.
-	 * 
-	 * @param icons 菜单包含的 Icon, 必须保证数组合法(数组大小等于菜单容量且所有元素非空).
-	 * @return
-	 */
-	public AbstractItem<?>[] init(Icon[] icons) {
-		int length = icons.length;
-		AbstractItem<?>[] items = new AbstractItem<?>[length];
-		for (int i = 0; i < length; i++) {
-			Icon icon = icons[i];
-			AbstractItem<?> item;	
-			if (icon == null) {
-				item = VirtualMenu.emptyItem();
-			} else if (icon instanceof MultiIcon) {
-				Icon realIcon = ((MultiIcon) icon).viewIcon(this);
-				item = realIcon.refreshItem(this);
-				viewIcon.put((MultiIcon) icon, realIcon);
-				viewItem.put(realIcon, item);
-			} else {
-				item = icon.refreshItem(this);
-				viewItem.put(icon, item);
-			}
-			fastAccess.set(i, item);
-			items[i] = item;
+	
+	public AbstractPacketOutWindowItems<?> createPacketWindowItemsForMenu() {
+		val packetWindowItems = packetAdapter.createPacketWindowItems();
+		packetWindowItems.setWindowId(menu.getWindowId());
+		packetWindowItems.setRawItems(getCachePacketMenuRawItems());
+		return packetWindowItems;
+	}
+	
+	public AbstractPacketOutSetSlot<?> createPacketOutSetSlotForMenu(int slot) {
+		val packetWindowSetSlot = packetAdapter.createPacketSetSlot();
+		packetWindowSetSlot.setWindowId(menu.getWindowId());
+		packetWindowSetSlot.setSlot(slot);
+		packetWindowSetSlot.setRawItem(getCachePacketMenuRawItem(slot));
+		return packetWindowSetSlot;
+	}
+	
+	public AbstractPacketOutWindowItems<?> createPacketWindowItemsForInventory() {
+		val packetWindowItems = packetAdapter.createPacketWindowItems();
+		packetWindowItems.setWindowId(0);
+		packetWindowItems.setRawItems(getCacheInventoryRawItems());
+		return packetWindowItems;
+	}
+	
+	public AbstractPacketOutSetSlot<?> createPacketOutSetSlotForInventory(int slot) {
+		val packetWindowSetSlot = packetAdapter.createPacketSetSlot();
+		packetWindowSetSlot.setWindowId(0);
+		packetWindowSetSlot.setSlot(slot);
+		packetWindowSetSlot.setRawItem(getCacheInventoryRawItem(slot));
+		return packetWindowSetSlot;
+	}
+	
+	public void handlePacket(AbstractPacketOutSetSlot<?> packet) {
+		if (packet.getWindowId() == 0) {
+			this.updateCacheInventoryRawItem(packet.getSlot(), packet.getRawItem());
 		}
-		return items;
+	}
+	
+	public void handlePacket(AbstractPacketOutWindowItems<?> packet) {
+		if (packet.getWindowId() == 0) {
+			this.updateCacheInventoryRawItems(packet.getRawItems());
+		}
 	}
 
 }
